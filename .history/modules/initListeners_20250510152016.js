e4f99c2c381662b4503d39e48cf5f42ee0571e22
Your Name <you@example.com>
2025-05-10 15:44:55 +0300

    API-3

---
+++ .history/modules/initListeners_20250510152016.js
@@ -0,0 +1,87 @@
+import { postComment } from './api.js'
+import { comments, updateComments } from './comments.js'
+import { sanitizeHtml } from './sanitizeHtml.js'
+
+export const initLikeListeners = (renderComments) => {
+    const likeButtons = document.querySelectorAll('.like-button')
+
+    for (const likeButton of likeButtons) {
+        likeButton.addEventListener('click', (event) => {
+            event.stopPropagation()
+
+            const index = likeButton.dataset.index
+            const comment = comments[index]
+
+            comment.likes = comment.isLikes
+                ? comment.likes - 1
+                : comment.likes + 1
+
+            comment.isLikes = !comment.isLikes
+
+            renderComments()
+        })
+    }
+}
+
+export const initReplyListeners = () => {
+    const text = document.getElementById('text-input')
+    const commentElements = document.querySelectorAll('.comment')
+
+    for (const commentElement of commentElements) {
+        commentElement.addEventListener('click', () => {
+            const currentComment = comments[commentElement.dataset.index]
+            text.value = `${currentComment.name}: ${currentComment.text}`
+        })
+    }
+}
+
+export const initAddCommentListener = (renderComments) => {
+    const name = document.getElementById('name-input')
+    const text = document.getElementById('text-input')
+    const addButton = document.querySelector('.add-form-button')
+
+    addButton.addEventListener('click', () => {
+        if (!name.value || !text.value) {
+            console.error('заполните форму')
+            return
+        }
+
+        document.querySelector('.form-loading').style.display = 'block'
+        document.querySelector('.add-form').style.display = 'none'
+
+        postComment(sanitizeHtml(name.value), sanitizeHtml(text.value))
+            .then((data) => {
+                document.querySelector('.form-loading').style.display = 'none'
+                document.querySelector('.add-form').style.display = 'flex'
+
+                updateComments(data)
+                renderComments()
+                name.value = ''
+                text.value = ''
+            })
+            .catch((error) => {
+                debugger
+                document.querySelector('.form-loading').style.display = 'none'
+                document.querySelector('.add-form').style.display = 'flex'
+
+                if (error.massage === "Failed to fetch") {
+                    
+                }
+
+                if (error.massage === 'Ошибка сервера') {
+                    alert('Ошибка сервера')
+                }
+                if (error.massage === 'Неверный запрос') {
+                    alert('Имя и комментарий должны быть не короче 3х символов')
+
+                    name.classList.add('-error')
+                    text.classList.add('-error')
+
+                    setTimeout(() => {
+                        name.classList.remove('-error')
+                        text.classList.remove('-error')
+                    }, 2000)
+                }
+            })
+    })
+}
